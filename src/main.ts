cfcbca7dd415f55db001af5dc694aa4d07f0fa1a
Renato Bezerra <renato-bgs@live.com>
2023-12-07 23:19:40 -0300

    up

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.PORT || 3000);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
