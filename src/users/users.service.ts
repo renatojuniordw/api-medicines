89c50c0a57e5124a1a1550c8dfec83fcc38fce27
Renato Bezerra <renato-bgs@live.com>
2023-12-19 23:26:35 -0300

    initial tests

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,20 +35,13 @@ export class UsersService {
     return user;
   }
 
-  async updateUser(updateUserDto: UpdateUserDto, id: string): Promise<User> {
-    const user = await this.findUserById(id);
-    const { name, email, role, status } = updateUserDto;
-    user.name = name ? name : user.name;
-    user.email = email ? email : user.email;
-    user.role = role ? role : user.role;
-    user.status = status === undefined ? user.status : status;
-    try {
-      await user.save();
+  async updateUser(updateUserDto: UpdateUserDto, id: string) {
+    const result = await this.userRepository.update({ id }, updateUserDto);
+    if (result.affected > 0) {
+      const user = await this.findUserById(id);
       return user;
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Erro ao salvar os dados no banco de dados',
-      );
+    } else {
+      throw new NotFoundException('Usuário não encontrado');
     }
   }
 
